5cca7fe968de7fffcbcebdbce59174e5c5cb8779
Christophe Gakwaya <c.gakwaya@alustudent.com>
2024-01-12 12:57:09 -0800

    updated

--- regular_expressions/5-beginning_and_end.rb
+++ regular_expressions/5-beginning_and_end.rb
@@ -1,15 +1,3 @@
 #!/usr/bin/env ruby
 
-# Check if the command-line argument matches the specified pattern
-def match_pattern(input)
-  pattern = /^h.n$/
-  if input.match?(pattern)
-    puts input
-  end
-end
-
-# Accept one argument from the command line
-if ARGV.length == 1
-  match_pattern(ARGV[0])
-end
-
+puts ARGV[0].scan(/^h.n$/).join
